3b07ebc37b68e2fcb5c4eeacd839c7f052b0fcf4
Kenji <thangnt284@gmail.com>
2017-10-25 09:58:12 +0700

    The Eyes Never Lie !

--- inc/functions.php
+++ inc/functions.php
@@ -1,58 +1,24 @@
 <?php
 
-function k2_theme_logo()
-{
-    global $opt_theme_options;
+/* Include the TGM_Plugin_Activation class.*/
+require_once(get_template_directory() . '/inc/libs/class-tgm-plugin-activation.php');
 
-    echo '<div class="main_logo">';
+/* lip material icons */
+require_once get_template_directory() . '/inc/libs/material-design.php';
 
-    if( $opt_theme_options['logo_type'] == 1 ) {
+/* lip font-awesome */
+require_once get_template_directory() . '/inc/libs/font-awesome.php';
 
-        $img = (!empty($opt_theme_options['main_logo']['url'])) ? $opt_theme_options['main_logo']['url'] : get_template_directory_uri().'/assets/images/logo.png';
-        echo '<a href="' . esc_url(home_url('/')) . '"><img alt="' .  get_bloginfo( "name" ) . '" src="' . esc_url($img) . '"></a>';
-    } elseif ($opt_theme_options['logo_type'] == 0 ){
+/* load list plugins */
+require_once(get_template_directory() . '/inc/options/require_plugins.php');
 
-        $text = (!empty($opt_theme_options['logo_text'])) ? $opt_theme_options['logo_text'] : get_bloginfo( "name" );
+/* load theme options. */
+require_once(get_template_directory() . '/inc/options/theme_options.php');
 
-        echo '<h2 class="site-title"><a href="' . esc_url( home_url( '/' )) . '" rel="home">' . esc_html($text) . '</a></h2>';
-    } else {
-        echo '<h2 class="site-title"><a href="' . esc_url( home_url( '/' )) . '" rel="home">' . get_bloginfo( "name" ) . '</a></h2>';
-        echo '<p class="site-description">' . get_bloginfo( "description" ) . '</p>';
-    }
+/* load page options */
+require_once(get_template_directory() . '/inc/options/page_options.php');
 
-    echo '</div>';
+/* load template functions */
+require_once(get_template_directory() . '/inc/functions_core.php');
 
-    k2_theme_sticky_logo();
-}
-
-function k2_theme_sticky_logo(){
-    global $opt_theme_options;
-
-    /* sticky off. */
-    if(empty($opt_theme_options['menu_sticky'])) {
-        return;
-    }
-
-    /* default logo. */
-    if(empty($opt_theme_options['sticky_logo_type']) || $opt_theme_options['sticky_logo_type'] == 'default') {
-        return;
-    }
-
-    echo '<div class="sticky_logo hide">';
-
-    if(isset($opt_theme_options['sticky_logo_type']) && $opt_theme_options['sticky_logo_type'] == 'img' && !empty($opt_theme_options['sticky_logo']['url'])) {
-        echo '<a href="' . esc_url(home_url('/')) . '"><img alt="' .  get_bloginfo( "name" ) . '" src="' . esc_url($opt_theme_options['sticky_logo']['url']) . '"></a>';
-    } elseif (isset($opt_theme_options['sticky_logo_type']) && $opt_theme_options['sticky_logo_type'] == 'text' && !empty($opt_theme_options['sticky_logo_text'])){
-        echo '<h1 class="site-title"><a href="' . esc_url( home_url( '/' )) . '" rel="home">' . esc_html($opt_theme_options['sticky_logo_text']) . '</a></h1>';
-
-        if(!empty($opt_theme_options['sticky_logo_text_sologan']))
-            echo '<p class="site-description">'.esc_html($opt_theme_options['sticky_logo_text_sologan']).'</p>';
-
-    } else {
-        echo '<h1 class="site-title"><a href="' . esc_url( home_url( '/' )) . '" rel="home">' . get_bloginfo( "name" ) . '</a></h1>';
-        echo '<p class="site-description">' . get_bloginfo( "description" ) . '</p>';
-    }
-
-    echo '</div>';
-}
 ?>
--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <form action="<?php echo esc_url( home_url( '/' ) ); ?>" class="searchform"  method="get">
  	<div class="wrap-search">
-  		<input type="text" class="form-search" name="s" value="" placeholder="<?php esc_html_e('Search...','book-junky'); ?>">
+  		<input type="text" class="form-search" name="s" value="" placeholder="<?php esc_html_e('Search...','k2-theme'); ?>">
  		<button type="submit" class="search-submit"><i class="fa fa-search" aria-hidden="true"></i></button>
  		<input type="hidden" name="post_type" value="post" />
  	</div>
