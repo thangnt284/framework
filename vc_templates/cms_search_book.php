18e64dc39af3d3df7dc4c7d5defb7f422cf9ebed
KP <kenzy194@gmail.com>
2017-11-03 15:02:09 +0700

    up

--- vc_templates/cms_search_book.php
+++ vc_templates/cms_search_book.php
@@ -1,4 +1,8 @@
-<?php echo "con cho";
-	var_dump($atts['fancybox_style']);
-	var_dump($atts['carousel_title']);
-?>
+<div id="k2-map-test" style="width: 100%; height:500px"></div>
+<script>
+    jQuery('#k2-map-test').K2Map(
+        {
+            zoom:1
+        }
+    );
+</script>
